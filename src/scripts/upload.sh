00ff812d0a60ac0329ae336b1e64630afd3b8748
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-20 17:24:00 -0400

    Add most of upload parameters

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -1,8 +1,18 @@
 Upload() {
 		echo "Uploading '${FILE_PATH}' to '${UPLOAD_PATH}'..."
+
+		clientModified="";
+		if [ -n "${CLIENT_MODIFIED}" ]; then
+			clientModified=", \"client_modified\": \"${CLIENT_MODIFIED}\"";
+		fi;
+
+		[[ "${AUTORENAME}" = "1" ]] && AUTORENAME="true" || AUTORENAME="false"
+		[[ "${MUTE}" = "1" ]] && MUTE="true" || MUTE="false"
+		[[ "${STRICT_CONFLICT}" = "1" ]] && STRICT_CONFLICT="true" || STRICT_CONFLICT="false"
+
 		verboseCurl true -X POST https://content.dropboxapi.com/2/files/upload \
 			--header "Authorization: Bearer ${DROPBOX_TOKEN}" \
-			--header "Dropbox-API-Arg: {\"path\": \"${UPLOAD_PATH}\", \"mode\": \"overwrite\"}" \
+			--header "Dropbox-API-Arg: {\"path\": \"${UPLOAD_PATH}\", \"mode\": \"${MODE}\", \"autorename\": ${AUTORENAME}, \"mute\": ${MUTE}, \"strict_conflict\": ${STRICT_CONFLICT}${clientModified}}" \
 			--header "Content-Type: application/octet-stream" \
 			--data-binary @"${FILE_PATH}"
 }
