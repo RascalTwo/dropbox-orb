5c0d40333fecffb5a17ef73b1a1f7062623ca2ad
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-20 16:14:10 -0400

    Match Dropbox parameter documentation
    Add `permanent` and `parent_rev` parameters to delete

--- src/scripts/delete.sh
+++ src/scripts/delete.sh
@@ -1,9 +1,22 @@
 Delete() {
-		echo "Deleting '${DELETE_PATH}'..."
-		verboseCurl true -X POST https://api.dropboxapi.com/2/files/delete_v2 \
+		prefix="";
+		if [[ "${PERMANENT}" = "1" ]]; then
+			urlPath="permanently_delete";
+			prefix="Permanently "
+		else
+			urlPath="delete_v2"
+		fi;
+
+		parentRev="";
+		if [ -n "${PARENT_REV}" ]; then
+			parentRev=", \"parent_rev\": \"${PARENT_REV}\"";
+		fi;
+
+		echo "${prefix}Deleting '${DELETE_PATH}'..."
+		verboseCurl true -X POST https://api.dropboxapi.com/2/files/${urlPath} \
 			--header "Authorization: Bearer ${DROPBOX_TOKEN}" \
 			--header "Content-Type: application/json" \
-			--data "{\"path\": \"${DELETE_PATH}\"}"
+			--data "{\"path\": \"${DELETE_PATH}\"${parentRev}}"
 }
 
 # Will not run if sourced for bats-core tests.
